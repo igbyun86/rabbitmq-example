9df81940a51e83d228f5cd5aab98fcb1238173e5
big <igbyun86@gmail.com>
2020-11-27 18:04:26 +0900

    Fix 안쓰는 import 제거

--- src/main/java/com/ig/rabbitmqex/object/ObjectSender.java
+++ src/main/java/com/ig/rabbitmqex/object/ObjectSender.java
@@ -1,14 +1,11 @@
 package com.ig.rabbitmqex.object;
 
 import com.ig.rabbitmqex.object.model.User;
-import com.ig.rabbitmqex.topic.TopicReceiver;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.util.Map;
 
 @Component
 public class ObjectSender {
--- src/test/java/com/ig/rabbitmqex/ObjectTest.java
+++ src/test/java/com/ig/rabbitmqex/ObjectTest.java
@@ -6,9 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @SpringBootTest
 public class ObjectTest {
 
